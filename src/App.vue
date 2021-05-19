f7ad40cbd9ce4d467d87857e06c197af6eb9d975
Edilson Gonzalez <edilson.gonzalez@hugoapp.com>
2021-05-18 19:01:12 -0600

    Estructura inicial

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="UNIR ZACATECAS" title="Soy el título"/>
   </div>
 </template>
 
